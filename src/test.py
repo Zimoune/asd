749592fb7235492a8d01b8cfae67c4329d58331b
ElProton <lefebvreryan@hotmail.com>
2016-02-02 11:22:45 +0000

    fin TP sauf reponse

--- src/test.py
+++ src/test.py
@@ -44,37 +44,54 @@ def negative_markers1(markers, positive):
         i = 0
         while i < len(positive) and compare_cpt(m, positive[i]) != 0:
             i += 1
-        if i < len(positive):
-            negative += [m]
+        if i == len(positive):
+            negative.append(m)
         i = 0
     return negative
 
 
 # STRATEGY 2
 def negative_markers2(markers, positive):
-    positive_l = sorting.merge_sort(positive , compare_cpt)
+    positive_l = sorting.merge_sort(positive, compare_cpt)
     negative = []
     for m in markers:
         a = 0
-        b = len(positive)
+        b = len(positive_l)-1
         find = False
-        while (not find) and a < b:
+        while (not find) and a <= b:
             mid = (a+b)//2
-            comp = compare_cpt(m, positive[mid])
+            comp = compare_cpt(m, positive_l[mid])
             if comp == 0:
                 find = True
-            elif comp == 1:
-                a = mid +1
+            elif comp == -1:
+                b = mid -1
             else:
-               b = mid -1
-        if find == False:
-            negative += [m]
+               a = mid +1
+        if not find:
+            negative.append(m)
     return negative
 
 
 # STRATEGY 3
 def negative_markers3(markers, positive):
     negative = []
+    m_sort = sorting.merge_sort(markers, compare_cpt)
+    p_sort = sorting.merge_sort(positive, compare_cpt)
+    i = 0
+    for m in m_sort:
+        find = False
+        while (not find) and i<len(positive):
+            cmp = compare_cpt(m , p_sort[i])
+            if cmp == 1 :
+                i += 1
+            elif cmp == 0:
+                find = True
+            else :
+                break
+
+        if find == False:
+            negative.append(m)
+
     return negative
         
 if __name__ == "__main__":
@@ -92,20 +109,34 @@ if __name__ == "__main__":
     cpt = 0
     print("Negative markers: %s" % (negative_markers1(markers,positive)))
     print("Nb. comparisons: %d" % (cpt))
-    cpt_strat1 = cpt
+
 
     # test stategy 2
     cpt = 0
     print("Negative markers: %s" % (negative_markers2(markers,positive)))
     print("Nb. comparisons: %d" % (cpt))
-    cpt_strat2 = cpt
+
+
     # test stategy 3
     cpt = 0
     print("Negative markers: %s" % (negative_markers3(markers,positive)))
     print("Nb. comparisons: %d" % (cpt))
-    cpt_strat3 = cpt
 
-    data = open("gnup_tab.txt","wr")
 
-    for i in range(1, 11):
-        data.write(str(m)+' '+str(p)+' '+ str(
+    data = open("gnup_tab_90.txt","w")
+
+
+    for p in range(0,m):
+        markers = experience.markers(m)
+        positive = experience.experience(p, markers)
+        cpt = 0
+        negative_markers1(markers,positive)
+        cpt_strat1 = cpt
+        cpt = 0
+        negative_markers2(markers,positive)
+        cpt_strat2 = cpt
+        cpt = 0
+        negative_markers3(markers,positive)
+        cpt_strat3 = cpt
+        data.write(str(m)+' '+str(p)+' '+ str(cpt_strat1)+' '+ str(cpt_strat2)+' '+ str(cpt_strat3)+"\n")
+
